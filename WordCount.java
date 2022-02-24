02c1a1c80200e6061038bf8c078bd5824f65aaf6
chewzzz <chewziqing@gmail.com>
2022-02-24 12:38:35 +0800

    Update

---
+++ WordCount.java
@@ -0,0 +1,28 @@
+//word counter using HashedMap
+
+package myExercises2;
+
+import java.util.*;
+
+public class WordCount {
+public static void main (String[]args)
+{
+	Scanner sc = new Scanner (System.in);
+	Map <Character,Integer> m = new HashMap <Character,Integer>();
+
+	System.out.print("Enter paragraph: ");
+	String p = sc.nextLine().trim().toLowerCase();
+	
+	for (int i=0;i< p.length(); i++)
+	{
+		if (m.containsKey(p.charAt(i)) )
+			m.put(p.charAt(i), m.get(p.charAt(i))+1);
+		else
+			m.put(p.charAt(i), 0);
+	}
+	
+	System.out.print("\n"+m);
+	sc.close();
+	
+}
+}
