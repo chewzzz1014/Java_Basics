51557b7d825d78e8c7cb07d607207a81a8eafec7
chewzzz <chewziqing@gmail.com>
2022-03-21 16:14:10 +0800

    Initial Commit

---
+++ oop/testCircle.java
@@ -0,0 +1,14 @@
+package oop;
+
+import java.io.PrintStream;
+
+public class testCircle {
+    public static void main (String[]args){
+
+        Circle circle1 = new Circle("circle 1");
+        circle1.printDetail();
+
+        Circle circle2 = new Circle (5);
+        circle2.printDetail();
+    }
+}
