67638851d519f9feee381343cca6e6018fc8b2fd
chewzzz <chewziqing@gmail.com>
2022-03-28 14:54:00 +0800

    Added more objects of class Book

--- Object_Oriented_SEM2/TestBook.java
+++ Object_Oriented_SEM2/TestBook.java
@@ -11,6 +11,13 @@ public class TestBook {
 				book2.getTitle(), book2.getPrice(), Book.getTotal());
 		
 		Book.addTotalPriceOfBook(40);
+		System.out.printf("Added new book (RM%.2f).\n", 40.0 );
 		System.out.printf("Current total price is RM %.2f.\n", Book.getTotal());
+		
+		Book book3 = new Book("Alice in the wonderland", 30);
+		System.out.printf("Title of book3 is \"%1s\". Price is RM%.2f and current total price is RM %.2f.\n",
+				book3.getTitle(), book3.getPrice(), Book.getTotal());
+		
+		System.out.printf("Total price is RM%.2f", Book.getTotal());
 	}
 }
