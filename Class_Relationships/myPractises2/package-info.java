23a6ea56127c79184de430a55a038cb0bdff2c06
chewzzz <chewziqing@gmail.com>
2022-04-08 14:25:10 +0800

    Added javadocs

---
+++ Class_Relationships/myPractises2/package-info.java
@@ -0,0 +1,16 @@
+
+
+/*
+ *Demonstrate relationship among classes which include association, aggregation, composition and inheritance.
+ *<p>
+ *
+ *All classes in this package are related to one another. The implementation of these classes along with creating classes instances are included in Main.java.
+ *
+ *Inspired and modified from lecture exercises in Object Oriented Programming course that I'm taking currently, in semester 2 of CS degree. 
+ *
+ *@see <a href = "https://cdn.jsdelivr.net/gh/chewzzz1014/file@b1ab3b5ff3755b0fca0fbee3ad20ee0cf73bf09a/UML.drawio.png">UML diagram of classess in this package</a>
+ *
+ *@author CHEW ZI QING
+ */
+
+package myPractises2;
