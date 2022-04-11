ffce177abc3e68a337003296108e9a7fc8523127
chewzzz <chewziqing@gmail.com>
2022-04-11 18:19:48 +0800

    Initial Commit

--- Object_Oriented_SEM2/code/Postgraduate.java
+++ Object_Oriented_SEM2/code/Postgraduate.java
@@ -1,5 +1,29 @@
 package code;
 
-public class Postgraduate {
+public class Postgraduate extends Student{
 
+	private String thesisTitle;
+	Postgraduate(){
+		// the no-args constructor of super class, Student will be called automatically
+	}
+	
+	Postgraduate(String thesisTitle){
+		// the no-args constructor of super class, Student will be called automatically
+		this.thesisTitle = thesisTitle;
+	}
+	
+	//accessor of private data field
+	public String getThesisTitle() {
+		return this.thesisTitle;
+	}
+	
+	//setter of private data field
+	public void setThesisTitle(String thesisTitle) {
+		this.thesisTitle = thesisTitle;
+	}
+	
+	// overriding method of super class's toString()
+	public String toString() {
+	 return "Postgraduate "+super.toString()+"\nThesis: "+this.getThesisTitle();
+	}
 }
--- Object_Oriented_SEM2/code/Undergraduate.java
+++ Object_Oriented_SEM2/code/Undergraduate.java
@@ -1,5 +1,31 @@
+// Week 4
+// CHEW ZI QING 212360
 package code;
 
-public class Undergraduate {
-
+public class Undergraduate extends Student{
+	private String classification;
+	
+	Undergraduate(){
+		// the no-args constructor of super class, Student will be called automatically
+	}
+	
+	Undergraduate(String classification){
+		// the no-args constructor of super class, Student will be called automatically
+		this.classification = classification;
+	}
+	
+	//accessor of private data field
+	public String getClassification() {
+		return this.classification;
+	}
+	
+	//setter of private data field
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+	
+	// overriding method of modified super class's toString()
+	public String toString() {
+		return "Undergraduate "+super.toString()+"\nClassification: "+this.getClassification();
+	}
 }
