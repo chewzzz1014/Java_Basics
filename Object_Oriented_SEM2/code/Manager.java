b7cbba04e42ef1229c085d0fc78c1bc219fcf8b3
chewzzz <chewziqing@gmail.com>
2022-04-11 15:49:19 +0800

    Initial Commit

---
+++ Object_Oriented_SEM2/code/Manager.java
@@ -0,0 +1,27 @@
+package code;
+
+public class Manager extends Staff {
+
+	private String department;
+	
+	Manager(){
+		System.out.println("In Manager constructor");
+	}
+	
+	public String getDepartment() {
+		return this.department;
+	}
+	
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public double bonus()	{		//overriding method
+		return 0.2*this.getSalary();
+	}
+	
+	public void printDetails() {
+		super.printDetails();
+		System.out.printf("Department: %1s",this.getDepartment());
+	}
+}
