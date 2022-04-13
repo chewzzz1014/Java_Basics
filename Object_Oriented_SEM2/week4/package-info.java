9e880c4ef44f91dec9f780ea31e672ddbfee8876
chewzzz <chewziqing@gmail.com>
2022-04-13 18:14:02 +0800

    Update package info

--- Object_Oriented_SEM2/week4/package-info.java
+++ Object_Oriented_SEM2/week4/package-info.java
@@ -1,1 +1,8 @@
+/*
+ * Consists of code assignments written for Object-Oriented Programming course in week 4 and 5, semester 2 of CS degree.
+ * <p>
+ * 
+ * @author CHEW ZI QING
+ */
+
 package week4;
