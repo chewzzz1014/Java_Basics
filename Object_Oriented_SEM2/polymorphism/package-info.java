5f5e0325f230df3423daf8c8cf488eba1e6c549c
chewzzz <chewziqing@gmail.com>
2022-04-25 20:11:38 +0800

    Added new package and package info

---
+++ Object_Oriented_SEM2/polymorphism/package-info.java
@@ -0,0 +1,4 @@
+/*
+ * This package contains my code practices as well as lab assignments for polymorphism concept in Object-Oriented Programming. This concept was taught in week 6 of semester 2 in my CS degree.
+ */
+package polymorphism;
