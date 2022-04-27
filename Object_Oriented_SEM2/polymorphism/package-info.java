855e7dd4dc247804fe2a976dd174675989af77a0
chewzzz <chewziqing@gmail.com>
2022-04-27 11:17:29 +0800

    Included link to UML diagram of classes

--- Object_Oriented_SEM2/polymorphism/package-info.java
+++ Object_Oriented_SEM2/polymorphism/package-info.java
@@ -2,13 +2,15 @@
  * This package contains my code practices as well as lab assignments for polymorphism concept in Object-Oriented Programming. This concept was taught in week 6 of semester 2 in my CS degree.
  * 
  * Classes that have relationships to each other:
+ * 
  * 1. - Car
  *    	- Caravan
  *    	- MVP
  *    	- Sedan
  *    -TestCar
  *    
- * 2. - Shape
+ * 2.  (refer the UML diagram for details https://cdn.jsdelivr.net/gh/chewzzz1014/file/n1.drawio.png )
+ *   - Shape
  * 	  	- TwoDimensional
  * 			- Circle
  * 			- Triangle
