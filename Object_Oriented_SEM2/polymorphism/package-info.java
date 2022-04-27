6835bfa7ca8b666ae5587119ff51287e5d091c74
chewzzz <chewziqing@gmail.com>
2022-04-27 11:44:04 +0800

    Added formulae of shapes

--- Object_Oriented_SEM2/polymorphism/package-info.java
+++ Object_Oriented_SEM2/polymorphism/package-info.java
@@ -18,6 +18,16 @@
  * 	  	- ThreeDimensional
  * 			- Cube
  * 			- Sphere
+ * 
+ * 
+ * Formulae for shapes:
+ *		circle area: pie r^2
+ *		triangle area: 0.5*b*h
+ *		square area: l*w
+ *		
+ *		cube: 6*(l^2), (l^3)
+ *		sphere: 4*pie*(r^2), (4/3)*pie*(r^2)
+ * 
  *
  */
 package polymorphism;
