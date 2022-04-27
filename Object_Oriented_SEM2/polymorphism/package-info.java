67e0113e7c73d301eb79abd84b39ec6326c5a5d7
chewzzz <chewziqing@gmail.com>
2022-04-27 11:14:50 +0800

    Updated package-info

--- Object_Oriented_SEM2/polymorphism/package-info.java
+++ Object_Oriented_SEM2/polymorphism/package-info.java
@@ -1,4 +1,21 @@
 /*
  * This package contains my code practices as well as lab assignments for polymorphism concept in Object-Oriented Programming. This concept was taught in week 6 of semester 2 in my CS degree.
+ * 
+ * Classes that have relationships to each other:
+ * 1. - Car
+ *    	- Caravan
+ *    	- MVP
+ *    	- Sedan
+ *    -TestCar
+ *    
+ * 2. - Shape
+ * 	  	- TwoDimensional
+ * 			- Circle
+ * 			- Triangle
+ * 			- Square
+ * 	  	- ThreeDimensional
+ * 			- Cube
+ * 			- Sphere
+ *
  */
 package polymorphism;
