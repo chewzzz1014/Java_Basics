ce299b75dcb8e864ca71d7a7c1ea4c06d9c578c2
chewzzz <chewziqing@gmail.com>
2022-05-11 23:09:39 +0800

    Added package into

--- Object_Oriented_SEM2/Lab5/package-info.java
+++ Object_Oriented_SEM2/Lab5/package-info.java
@@ -1,1 +1,9 @@
+/*
+ * Code I've written in Week 7 of Semester 2 during my CS degree.
+ * 
+ *  I attempted to apply different way of try and catch exception in the code I've written few weeks ago. 
+ *  
+ *  This gave me basic exposure to Exception Handling in Java. 
+ *  I will implement it in all of my code (Sem 1 and Week 1-6 Sem 2) when I am completely understood of this topic and master it well.
+ */
 package Lab5;
