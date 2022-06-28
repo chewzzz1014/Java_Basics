90b4002c0a8904096752a38fbe80321165d1b448
chewzzz <chewziqing@gmail.com>
2022-06-28 14:37:27 +0800

    Removed safePop

--- Object_Oriented_SEM2/generic/GenericStack.java
+++ Object_Oriented_SEM2/generic/GenericStack.java
@@ -4,7 +4,9 @@ import java.util.*;
  * 	(Implement GenericStack using inheritance) In lecture notes, GenericStack and ArrayList are implemented using composition. Define a new GenericStack class that extends ArrayList. Draw the UML diagram for the classes then implement GenericStack. Write a test program that prompts the user to enter five strings and displays them in reverse order
  */
 public class GenericStack <E> extends ArrayList<E> {
+
 	private ArrayList<E> list = new ArrayList<E>();
+	
 		GenericStack(){
 			
 		}
