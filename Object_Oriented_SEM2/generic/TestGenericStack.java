574727cf0462a0d0f83d4ffcdda9f48f89c774a5
chewzzz <chewziqing@gmail.com>
2022-06-28 15:27:22 +0800

    Implemented pop and peek. Added comments

--- Object_Oriented_SEM2/generic/TestGenericStack.java
+++ Object_Oriented_SEM2/generic/TestGenericStack.java
@@ -12,10 +12,16 @@ public class TestGenericStack {
 		System.out.println("*****Enter 5 string*****");
 		for (int i=0; i<5; i++) {
 			System.out.printf("\n#%1d: ", (i+1));
-			stack.push(sc.next());
-			sc.next();
+			stack.push(sc.nextLine());
+			//sc.nextLine();
 		}
+		// trying to peek and push
+		System.out.printf("\n\nCurrent Last Element in GenericStack: %1s\nRemoving Last Element [%1s]...\nAdding \"Queen\" to GenericStack...\n", stack.peek(), stack.pop());
+		stack.push("Queen");
+		System.out.printf("Current Last Element in GenericStack: %1s\n", stack.peek());
+
+		// print all the properties of GenericStack
 		stack.printProperties();
-		
+		sc.close();
 	}
 }
