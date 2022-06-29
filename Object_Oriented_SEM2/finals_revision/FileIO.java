8bd008959827da51c43dfebe00c7cdcb7d44961d
chewzzz <chewziqing@gmail.com>
2022-06-29 17:57:44 +0800

    Initial Commit

---
+++ Object_Oriented_SEM2/finals_revision/FileIO.java
@@ -0,0 +1,11 @@
+package finals_revision;
+import java.util.*;
+/*
+ * Given a Scanner reference variable, stdin, that is associated with standard input, read in two words. Use the first word as the name of a file to create and store the second word in. Make sure that the data written to the file has been flushed from its buffer and that any system resources used during the course of running these statements have been released. (Do not concern yourself with any possible exceptions here-- assume they are handled elsewhere.)
+
+ */
+public class FileIO {
+	public static void main (String[]args) {
+		
+	}
+}
