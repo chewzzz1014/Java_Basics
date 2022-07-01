2e9c028db5eff4bc98f19efa4a834d2336eb9b4c
chewzzz <chewziqing@gmail.com>
2022-07-01 13:16:49 +0800

    Completed

--- Object_Oriented_SEM2/finals_revision/CommDevice.java
+++ Object_Oriented_SEM2/finals_revision/CommDevice.java
@@ -1,11 +1,7 @@
 package finals_revision;
 
-/*
- * transmit: accepts two string parameters (destination and text) and returns nothing
-receive: accepts two string parameters (receiver and text) and returns a boolean
-
- */
 public interface CommDevice {
-	public static void transmit(String destination, String text);
-	public static boolean receive(String receiver, String text);
+	// no abstract keyword needed. By default methods in interface are abstract
+	public void transmit(String destination, String text);
+	public boolean receive(String receiver, String text);
 }
--- Object_Oriented_SEM2/finals_revision/Firewall.java
+++ Object_Oriented_SEM2/finals_revision/Firewall.java
@@ -1,5 +1,31 @@
 package finals_revision;
 
-public class Firewall {
-
+public class Firewall implements CommDevice {
+	private String permittedReceiver, buffer, destination;
+	
+	Firewall(String permittedReceiver){
+		this.permittedReceiver = permittedReceiver;
+	}
+	
+	public String getDestination() {
+		return this.destination;
+	}
+	
+	public void transmit(String destination, String text) {
+		this.destination = destination;
+		this.buffer = text;
+		System.out.printf("Date scheduled for transmission to %1s\n", this.getDestination());
+	}
+	
+	public boolean receive(String receiver, String text) {
+		if (this.permittedReceiver.equals(receiver)) {
+			this.buffer = text;
+			return true;
+		}
+		else {
+			this.buffer = "";
+			System.out.printf("Attempted breach of firewall by %1s\n", receiver);
+			return false;
+		}
+	}
 }
