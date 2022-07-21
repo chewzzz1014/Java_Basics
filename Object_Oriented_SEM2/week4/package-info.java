ff851ccd8f6f9778c4dcb07996a9643d9c64704b
chewzzz1014 <92832451+chewzzz1014@users.noreply.github.com>
2022-07-21 17:05:38 +0800

    Delete package-info.java

--- Object_Oriented_SEM2/week4/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Consists of code assignments written for Object-Oriented Programming course in week 4 and 5, semester 2 of CS degree.
- * <p>
- * 
- * @author CHEW ZI QING
- */
-
-package week4;
