1c1532a7ae9b3c19d4511411f597edb2e4036cc0
chewzzz1014 <92832451+chewzzz1014@users.noreply.github.com>
2022-07-21 16:52:58 +0800

    Delete package-info.java

--- Object_Oriented_SEM2/code/package-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * Consists of code exercises and lab assignments written for Object-Oriented Programming course in semester 2 of CS degree.
- * <p>
- * 
- * @author CHEW ZI QING
- */
-package code;
