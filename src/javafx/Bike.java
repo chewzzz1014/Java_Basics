b86e439cb1406ba00b88f9607aaa0141200ac3ad
chewzzz <chewziqing@gmail.com>
2022-10-25 11:39:35 +0800

    Remove excessive 'this' in class

--- src/javafx/Bike.java
+++ src/javafx/Bike.java
@@ -7,26 +7,26 @@ public class Bike {
 	public Bike(double car_speed, double speed_limit) {
 		this.car_speed = car_speed;
 		this.speed_limit = speed_limit;
-		this.calcFine();
+		calcFine();
 	}
 	
 	public double getCarSpeed() {
-		return this.car_speed;
+		return car_speed;
 	}
 	
 	public double getSpeedLimit() {
-		return this.speed_limit;
+		return speed_limit;
 	}
 	
 	public void calcFine() {
 		if (this.car_speed <= this.speed_limit) 
-			this.fine = 0;
+			fine = 0;
 		else 
-			this.fine = 30 + (this.car_speed - this.speed_limit);
+			fine = 30 + (this.car_speed - this.speed_limit);
 	}
 	
 	public double getFine() {
-		return this.fine;
+		return fine;
 	}
 
 }
--- src/javafx/Car.java
+++ src/javafx/Car.java
@@ -6,26 +6,26 @@ public class Car {
 	public Car(double car_speed, double speed_limit) {
 		this.car_speed = car_speed;
 		this.speed_limit = speed_limit;
-		this.calcFine();
+		calcFine();
 	}
 	
 	public double getCarSpeed() {
-		return this.car_speed;
+		return car_speed;
 	}
 	
 	public double getSpeedLimit() {
-		return this.speed_limit;
+		return speed_limit;
 	}
 	
 	public void calcFine() {
 		if (this.car_speed <= this.speed_limit) 
-			this.fine = 0;
+			fine = 0;
 		else 
-			this.fine = (this.car_speed - this.speed_limit) * (this.car_speed - this.speed_limit) * 0.5;
+			fine = (car_speed - speed_limit) * (car_speed - speed_limit) * 0.5;
 	}
 	
 	public double getFine() {
-		return this.fine;
+		return fine;
 	}
 	
 }
