e96b7ea23cebb81b8e74ab51dd9880e6a532dc8b
chewzzz <chewziqing@gmail.com>
2022-11-03 09:37:56 +0800

    Complete Circle Motion

--- src/javafx/CircleMotion.java
+++ src/javafx/CircleMotion.java
@@ -3,24 +3,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import java.lang.*;
+import javafx.event.*;
+import javafx.scene.input.MouseEvent;
+import java.util.Date;
+import javafx.scene.text.Text;
 
 public class CircleMotion extends Application{
+		// to be read and modified by inner class (mouse event handler)
+		private int sceneWidth = 280, sceneHeight = 200;
+		private int count = 0;
+		private Date start, end;
+		private long diff;
+		private Circle circle = new Circle();
+		
 	public void start(Stage primaryStage) {
-		
-		int sceneWidth = 280, sceneHeight = 200;
-		
-		Circle circle = new Circle();
+		// start to count the time when the moment program is ran
+		start = new Date();
+		// set radius, center coordinates and color of circle. Coordinates and color are random
 		circle.setCenterX(randomCoorX(sceneWidth));
 		circle.setCenterY(randomCoorY(sceneHeight));
 		circle.setRadius(10);
 		circle.setFill(Color.rgb(random255(), random255(), random255()));
 		
+		// add mouse event handler for circle
+		circle.setOnMouseClicked(new CircleHandler());
+		
+		// add circle to pane
 		Pane pane = new Pane();
 		pane.getChildren().add(circle);
 		
+		// set up scene and show scene
 		Scene scene = new Scene(pane, sceneWidth, sceneHeight);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Circle Motion");
@@ -30,10 +45,14 @@ public class CircleMotion extends Application{
 		launch(args);
 	}
 	
+	// generate random number from 0 to 255 (both inclusively)
+	// for generating random color
 	public static int random255() {
 		return (int)Math.floor(Math.random()*256);
 	}
 	
+	// generate random x coordinates for center of circle
+	// can be used for any width of scene
 	public static int randomCoorX(int sceneWidth) {
 		// center can be positioned between (inclusively) 10 and 270
 		// formula: floor(random() * (max-min)) + min
@@ -41,9 +60,43 @@ public class CircleMotion extends Application{
 		return (int)Math.floor(Math.random()*(max-min+1)) + min;
 	}
 	
+	// generate random y coordinates for center of circle
 	public static int randomCoorY(int sceneHeight) {
 		// center can be positioned between (inclusively) 10 and 180
 		int min = 10, max = sceneHeight-10;
 		return (int)Math.floor(Math.random()*(max-min+1)) + min;
 	}
+	
+	
+	// inner class: event handler for clicking on circle
+	class CircleHandler implements EventHandler<MouseEvent>{
+		public void handle(MouseEvent e) {
+			
+			// keep changing the coordinates and colour of circle
+			if (count < 20) {
+				circle.setCenterX(randomCoorX(sceneWidth));
+				circle.setCenterY(randomCoorY(sceneHeight));
+				circle.setRadius(10);
+				circle.setFill(Color.rgb(random255(), random255(), random255()));
+				count ++;
+			}else if (count==20) {
+				
+				// count the time difference between start time and end time (after 20 circles clicked)
+				// show the time spent on a new window
+				end = new Date();
+				diff = end.getTime() - start.getTime();
+				String output = "Time spent is " + diff + " milliseconds";
+				BorderPane pane = new BorderPane();
+				pane.setCenter(new Text(output));
+				
+				Scene scene = new Scene(pane, sceneWidth, sceneHeight);
+				Stage stage = new Stage();
+				stage.setScene(scene);
+				stage.setTitle("Result");
+				stage.show();
+			}
+		}
+		
+	}
+	
 }
