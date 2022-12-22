3baa27b20c1720fde181092b1855424f78b48c55
chewzzz <chewziqing@gmail.com>
2022-12-22 09:09:36 +0800

    Successfully ran example program

--- src/RMIClient.java
+++ src/RMIClient.java
@@ -1,84 +1,76 @@
-import java.io.*;
-import java.net.*;
+
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class RMIClient extends Application {
-	 // IO streams
-	  DataOutputStream toServer = null;
-	  DataInputStream fromServer = null;
+	
+	private RMIServerInterface student;
 
-	  @Override // Override the start method in the Application class
+	  private Button btGetScore = new Button("Get Score");
+	  private TextField tfName = new TextField();
+	  private TextField tfScore = new TextField();
+
 	  public void start(Stage primaryStage) {
-	    // Panel p to hold the label and text field
-	    BorderPane paneForTextField = new BorderPane();
-	    paneForTextField.setPadding(new Insets(5, 5, 5, 5)); 
-	    paneForTextField.setStyle("-fx-border-color: green");
-	    paneForTextField.setLeft(new Label("Enter a radius: "));
-	    
-	    TextField tf = new TextField();
-	    tf.setAlignment(Pos.BOTTOM_RIGHT);
-	    paneForTextField.setCenter(tf);
-	    
-	    BorderPane mainPane = new BorderPane();
-	    // Text area to display contents
-	    TextArea ta = new TextArea();
-	    mainPane.setCenter(new ScrollPane(ta));
-	    mainPane.setTop(paneForTextField);
-	    
-	    // Create a scene and place it in the stage
-	    Scene scene = new Scene(mainPane, 450, 200);
-	    primaryStage.setTitle("Client"); // Set the stage title
+	    GridPane gridPane = new GridPane();
+	    gridPane.setHgap(5);
+	    gridPane.add(new Label("Name"), 0, 0);
+	    gridPane.add(new Label("Score"), 0, 1);
+	    gridPane.add(tfName, 1, 0);
+	    gridPane.add(tfScore, 1, 1);
+	    gridPane.add(btGetScore, 1, 2);
+
+	    // Create a scene and place the pane in the stage
+	    Scene scene = new Scene(gridPane, 250, 250);
+	    primaryStage.setTitle("StudentServerInterfaceClient"); 
 	    primaryStage.setScene(scene); // Place the scene in the stage
 	    primaryStage.show(); // Display the stage
 	    
-	    tf.setOnAction(e -> {
-	      try {
-	        // Get the radius from the text field
-	        double radius = Double.parseDouble(tf.getText().trim());
-	  
-	        // Send the radius to the server
-	        toServer.writeDouble(radius);
-	        toServer.flush();
-	  
-	        // Get area from the server
-	        double area = fromServer.readDouble();
-	  
-	        // Display to the text area
-	        ta.appendText("Radius is " + radius + "\n");
-	        ta.appendText("Area received from the server is "
-	          + area + '\n');
-	      }
-	      catch (IOException ex) {
-	        System.err.println(ex);
-	      }
-	    });
-	  
+	    initializeRMI();
+	    btGetScore.setOnAction(e -> getScore());
+	  }
+
+	  private void getScore() {
 	    try {
-	      // Create a socket to connect to the server
-	      Socket socket = new Socket("localhost", 8000);
-	      // Socket socket = new Socket("130.254.204.36", 8000);
-	      // Socket socket = new Socket("drake.Armstrong.edu", 8000);
+	      // Get student score
+	      double score = student.findScore(tfName.getText().trim());
 
-	      // Create an input stream to receive data from the server
-	      fromServer = new DataInputStream(socket.getInputStream());
-
-	      // Create an output stream to send data to the server
-	      toServer = new DataOutputStream(socket.getOutputStream());
+	      // Display the result
+	      if (score < 0)
+	        tfScore.setText("Not found");
+	      else
+	        tfScore.setText(score+"");
 	    }
-	    catch (IOException ex) {
-	      ta.appendText(ex.toString() + '\n');
+	    catch(Exception ex) {
+	      ex.printStackTrace();
 	    }
 	  }
 
+	  /** Initialize RMI */
+	  protected void initializeRMI() {
+	    String host = "";
+
+	    try {
+	      Registry registry = LocateRegistry.getRegistry(host);
+	      student = (RMIServerInterface)
+	        registry.lookup("RMIServerInterfaceImpl");
+	      System.out.println("Server object " + student + " found");
+	    }
+	    catch(Exception ex) {
+	      System.out.println(ex);
+	    }
+	  }
+
+	  /**
+	   * The main method is only needed for the IDE with limited
+	   * JavaFX support. Not needed for running from the command line.
+	   */
 	  public static void main(String[] args) {
 	    launch(args);
 	  }
--- src/RegisterWithRMIServer.java
+++ src/RegisterWithRMIServer.java
@@ -1,4 +1,14 @@
+import java.rmi.registry.*;
 
 public class RegisterWithRMIServer {
-
+	public static void main(String[]args) {
+		try {
+			RMIServerInterface obj = new RMIServerInterfaceImpl();
+			Registry registry = LocateRegistry.getRegistry();
+			registry.rebind("RMIServerInterfaceImpl", obj);
+			System.out.println("Student server " + obj + " registered");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
