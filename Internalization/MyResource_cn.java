9b2030274a0b2bf1bdbe8fd54bd74f73ac9d4160
chewzzz <chewziqing@gmail.com>
2023-01-11 21:12:38 +0800

    Add chinese Resource Bundle

---
+++ Internalization/MyResource_cn.java
@@ -0,0 +1,11 @@
+import java.util.ListResourceBundle;
+
+public class MyResource_cn extends ListResourceBundle{
+	protected Object[][] getContents(){
+		return new Object[][] {
+			{"Student", "学生"},
+			{"has", "拥有"},
+			{"correct count", "正确答案"}
+		};
+	}
+}
