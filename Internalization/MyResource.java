017027b2c7b0521b04cddcb2b22a63261db642ea
chewzzz <chewziqing@gmail.com>
2023-01-11 20:55:41 +0800

    Add Resource Bundle

---
+++ Internalization/MyResource.java
@@ -0,0 +1,9 @@
+import java.util.ListResourceBundle;
+
+public class MyResource extends ListResourceBundle{
+	protected Object[][] getContents(){
+		return new Object[][] {
+			{"Student", "Student"}
+		};
+	}
+}
---
+++ Internalization/MyResource_ms.java
@@ -0,0 +1,9 @@
+import java.util.ListResourceBundle;
+
+public class MyResource_ms extends ListResourceBundle{
+	protected Object[][] getContents(){
+		return new Object[][] {
+			{"Student", "Pelajar"}
+		};
+	}
+}
