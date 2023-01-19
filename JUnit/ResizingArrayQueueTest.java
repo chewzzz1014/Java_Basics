d5bcef14d79cffa39dcac5713fee1fea4cd1b242
chewzzz <chewziqing@gmail.com>
2023-01-19 09:46:32 +0800

    Add method header for all tests

---
+++ JUnit/ResizingArrayQueueTest.java
@@ -0,0 +1,49 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ResizingArrayQueueTest {
+
+	ResizingArrayQueue<String> queue = new ResizingArrayQueue<String>();
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testResize() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEnqueue() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDequeue() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testIterator() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
